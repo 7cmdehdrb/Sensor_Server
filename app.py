3c6dc2f81c61a4ca63f33eddfe38391c929f0ca0
7cmdehdrb <7cmdehdrb@naver.com>
2021-10-04 17:05:29 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import serial
+import threading
+from time import sleep
+
+
+class Bluetooth(object):
+    def __init__(self, port_num):
+
+        self.port_num = port_num
+        self.light_value = 0
+
+        self.ser = serial.Serial(
+            port=port_num,
+            baudrate=9600,
+        )
+
+        self.th = threading.Thread(target=self.receive_data)
+        self.th.daemon = True
+        self.th.start()
+
+    def receive_data(self):
+        line = []
+
+        while True:
+            try:
+                data = self.ser.read()
+
+                line.append(data)
+
+                if len(line) >= 6:
+                    if line[-2] == "\n".encode():
+                        self.handleData(line)
+                        del line[:]
+
+            except Exception as ex:
+                print(ex)
+
+    def handleData(self, line):
+        self.light_value = ord(line[-1])
+
+
+if __name__ == "__main__":
+    bt = Bluetooth("COM4")
+
+    while True:
+        print(bt.light_value)
+        sleep(0.1)
